6373892bcae12487a6741ce6f545608b62068fab
Pablo Ancheta <pabloancheta@gmail.com>
2018-06-22 12:43:35 -0300

    Ajuste modelo usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->bigInteger('celular');
+            $table->string('email')->unique()->nullable();
             $table->string('password');
+            $table->float('saldo', 3, 2);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+        
+        DB::table('users')->insert([
+        	'name' => 'Pablo',
+        	'celular' => 2215429960,
+        	'email' => null,
+        	'password' => bcrypt ('1234'),
+        	'saldo' => 0,
+            'remember_token' => str_random(10),
+		]);
+
+		DB::table('users')->insert([
+        	'name' => 'Javier',
+        	'celular' => 2214951946,
+        	'email' => null,
+        	'password' => bcrypt ('1234'),
+        	'saldo' => 0,
+            'remember_token' => str_random(10),
+		]);
+
+		DB::table('users')->insert([
+        	'name' => 'Diego',
+        	'celular' => 1159803008,
+        	'email' => null,
+        	'password' => bcrypt ('1234'),
+        	'saldo' => 0,
+            'remember_token' => str_random(10),
+		]);
+
+		DB::table('users')->insert([
+        	'name' => 'Marcelo',
+        	'celular' => 2214593156,
+        	'email' => null,
+        	'password' => bcrypt ('1234'),
+        	'saldo' => 0,
+            'remember_token' => str_random(10),
+		]);
+    }
+}
